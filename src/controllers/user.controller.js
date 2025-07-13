5753af5878ddee1e2524af37b48e7395a55d814e
Piyush Sankhala <piyushsankhala2004@gmail.com>
2025-07-13 14:57:18 +0530

    gg

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -191,7 +191,7 @@ const getallusers = async (req, res) => {
     // ✅ filter out nulls
     const validUsers = chatusers.filter(Boolean);
 
-    return res.status(200).json(validUsers);
+    return res.status(200).json({validUsers});
   } catch (err) {
     console.error(err);
     return res.status(500).json({ message: "Server error" });
